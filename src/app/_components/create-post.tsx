5872f05ad8f819514f6725cf70b5439122d78f82
kira-0521 <kiratanaka0521@gmail.com>
2024-03-03 17:10:48 +0900

    feat: mantine useForm

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,34 +1,42 @@
 "use client";
 
 import { Button, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { zodResolver } from "mantine-form-zod-resolver";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { z } from "zod";
 
 import { api } from "~/trpc/react";
 
+const schema = z.object({
+	name: z.string().min(2, { message: "Name should have at least 2 letters" }),
+});
+
 export function CreatePost() {
 	const router = useRouter();
-	const [name, setName] = useState("");
+	const form = useForm({
+		initialValues: {
+			name: "",
+		},
+		validate: zodResolver(schema),
+	});
 
 	const createPost = api.post.create.useMutation({
 		onSuccess: () => {
 			router.refresh();
-			setName("");
 		},
 	});
 
 	return (
 		<form
-			onSubmit={(e) => {
-				e.preventDefault();
-				createPost.mutate({ name });
-			}}
+			onSubmit={form.onSubmit((values) =>
+				createPost.mutate({ name: values.name }),
+			)}
 		>
 			<TextInput
 				type="text"
 				placeholder="Title"
-				value={name}
-				onChange={(e) => setName(e.target.value)}
+				{...form.getInputProps("name")}
 			/>
 			<Button type="submit" disabled={createPost.isLoading}>
 				{createPost.isLoading ? "Submitting..." : "Submit"}
