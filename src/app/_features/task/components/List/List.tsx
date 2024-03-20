f5bc6083b8748c3c73cfa9667aba3a47d7fb4b05
kira-0521 <kiratanaka0521@gmail.com>
2024-03-20 16:00:39 +0900

    fix dnd

--- src/app/_features/task/components/List/List.tsx
+++ src/app/_features/task/components/List/List.tsx
@@ -1,60 +1,137 @@
 "use client";
 
-import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
-import { Text, rem } from "@mantine/core";
-import { useListState } from "@mantine/hooks";
-import type { Task } from "@prisma/client";
-import { IconGripVertical } from "@tabler/icons-react";
+import {
+	DragDropContext,
+	Draggable,
+	type DropResult,
+	Droppable,
+} from "@hello-pangea/dnd";
+import type { Status, Task } from "@prisma/client";
+
 import cx from "clsx";
 import { useRouter } from "next/navigation";
-import type { FC } from "react";
+import { type FC, useState } from "react";
 import classes from "./index.module.css";
 
 type Props = {
 	taskList: Task[];
+	statusList: Status[];
 };
 
-export const TaskList: FC<Props> = ({ taskList }) => {
-	const router = useRouter();
-	const [state, handlers] = useListState(taskList);
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (
+	source: List,
+	destination: List,
+	droppableSource: DropResult["source"],
+	droppableDestination: DropResult["destination"],
+) => {
+	const sourceClone = Array.from(source);
+	const destClone = Array.from(destination);
+	const [removed] = sourceClone.splice(droppableSource.index, 1);
 
-	const items = state.map((item, index) => (
-		<Draggable key={item.id} index={index} draggableId={item.id.toString()}>
-			{(provided, snapshot) => (
-				<div
-					className={cx(classes.item, {
-						[classes.itemDragging ?? ""]: snapshot.isDragging,
-					})}
-					onClick={() => router.push(`/task/${item.id}`)}
-					ref={provided.innerRef}
-					{...provided.draggableProps}
-				>
-					<div {...provided.dragHandleProps} className={classes.dragHandle}>
-						<IconGripVertical
-							style={{ width: rem(18), height: rem(18) }}
-							stroke={1.5}
-						/>
-					</div>
-					<Text>{item.title}</Text>
-				</div>
-			)}
-		</Draggable>
-	));
+	if (!droppableDestination || !removed) return;
+	destClone.splice(droppableDestination.index, 0, removed);
+
+	const result = {} as Record<number, List>;
+	result[Number.parseInt(droppableSource.droppableId, 10)] = sourceClone;
+	result[Number.parseInt(droppableDestination.droppableId, 10)] = destClone;
+
+	return result;
+};
+
+const reorder = (list: List, startIndex: number, endIndex: number) => {
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	if (!removed) return;
+	result.splice(endIndex, 0, removed);
+
+	return result;
+};
+
+type List = Task[];
+export const TaskList: FC<Props> = ({ taskList, statusList }) => {
+	const router = useRouter();
+	const [state, setState] = useState(
+		statusList.map((s) => taskList.filter((t) => t.statusId === s.id)),
+	);
+
+	const handleDragEnd = (result: DropResult) => {
+		const { source, destination } = result;
+
+		// dropped outside the list
+		if (!destination) {
+			return;
+		}
+
+		const sIdx = Number.parseInt(source.droppableId, 10);
+		const dIdx = Number.parseInt(destination.droppableId, 10);
+
+		if (sIdx === dIdx) {
+			// vertical
+			const target = state[sIdx];
+			if (!target) return;
+			const items = reorder(target, source.index, destination.index);
+			const newState = [...state];
+			if (!items) return;
+			newState[sIdx] = items;
+
+			setState(newState);
+		} else {
+			// horizontal
+			const [srcLi, destLi] = [state[sIdx], state[dIdx]];
+			if (!srcLi || !destLi) return;
+			const result = move(srcLi, destLi, source, destination);
+			if (!result) return;
+			const newState = [...state];
+			const [srcLiResult, destLiResult] = [result[sIdx], result[dIdx]];
+			if (!srcLiResult || !destLiResult) return;
+			newState[sIdx] = srcLiResult;
+			newState[dIdx] = destLiResult;
+
+			setState(newState.filter((group) => group.length));
+		}
+	};
 
 	return (
-		<DragDropContext
-			onDragEnd={({ destination, source }) =>
-				handlers.reorder({ from: source.index, to: destination?.index || 0 })
-			}
-		>
-			<Droppable droppableId="dnd-list" direction="vertical">
-				{(provided) => (
-					<div {...provided.droppableProps} ref={provided.innerRef}>
-						{items}
-						{provided.placeholder}
-					</div>
-				)}
-			</Droppable>
+		<DragDropContext onDragEnd={handleDragEnd}>
+			{statusList.map((el, idx) => (
+				<Droppable key={el.id} droppableId={`${idx}`}>
+					{(provided, snapshot) => (
+						<div
+							ref={provided.innerRef}
+							className={cx(classes.board, {
+								[classes.boardDragOver ?? ""]: snapshot.isDraggingOver,
+							})}
+							{...provided.droppableProps}
+						>
+							{(state[idx] ?? []).map((card, index) => (
+								<Draggable
+									key={card.id}
+									draggableId={card.id.toString()}
+									index={index}
+								>
+									{(provided, snapshot) => (
+										<div
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}
+											className={cx(classes.card, {
+												[classes.cardDragging ?? ""]: snapshot.isDragging,
+											})}
+											onClick={() => router.push(`/task/${card.id}`)}
+										>
+											<div className={classes.dragHandle}>{card.title}</div>
+										</div>
+									)}
+								</Draggable>
+							))}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			))}
 		</DragDropContext>
 	);
 };
--- src/app/task/_components/Dashboard.tsx
+++ src/app/task/_components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex, Title } from "@mantine/core";
+import { Flex } from "@mantine/core";
 
 import type { FC } from "react";
 import { api } from "~/trpc/server";
@@ -11,41 +11,14 @@ type Props =
 			createType?: never;
 	  };
 
-const NOT_EXISTENT_ID = 0;
 export const Dashboard: FC<Props> = async ({ isOpenModal, createType }) => {
 	const statuses = await api.status.getAll.query();
 	const tasks = await api.task.getAll.query();
 
-	const taskStatusMap = tasks.reduce<Record<number, (typeof tasks)[number][]>>(
-		(acc, task) => {
-			const statusId = task.statusId ?? NOT_EXISTENT_ID;
-			if (!acc[statusId]) {
-				acc[statusId] = [];
-			}
-			acc[statusId] = [...(acc[statusId] ?? []), task];
-			return acc;
-		},
-		{ [NOT_EXISTENT_ID]: [] },
-	);
-
-	tasks[Symbol.iterator] = function* () {
-		yield* this;
-	};
-
 	return (
 		<>
 			<Flex gap={20}>
-				{statuses
-					.filter((s) => s.id !== NOT_EXISTENT_ID)
-					.sort((a, b) => a.displayOrder - b.displayOrder)
-					.map((status) => (
-						<Flex key={status.id} direction="column" miw={300}>
-							<Title order={2} lineClamp={1}>
-								{status.title}
-							</Title>
-							<TaskList taskList={taskStatusMap[status.id] ?? []} />
-						</Flex>
-					))}
+				<TaskList taskList={tasks} statusList={statuses} />
 			</Flex>
 			<CreateModal isOpen={isOpenModal} createType={createType ?? "manual"} />
 		</>
