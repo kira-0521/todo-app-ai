a2129e7e67b6f0c7b02a19b8c33cec773a76869b
kira-0521 <kiratanaka0521@gmail.com>
2024-03-26 20:27:39 +0900

    styling 修正

--- src/app/_components/skelton/DashBoardSkelton/index.tsx
+++ src/app/_components/skelton/DashBoardSkelton/index.tsx
@@ -1,25 +1,39 @@
-"use client";
-
 import { SimpleGrid, Skeleton, Stack } from "@mantine/core";
 import { type FC, memo } from "react";
+import classes from "./index.module.css";
 
 export const DashBoardSkelton: FC = memo(() => {
 	return (
-		<SimpleGrid cols={3}>
-			<Stack miw={300}>
-				<Skeleton height={50} radius="md" />
-				<Skeleton height={50} radius="md" />
-				<Skeleton height={50} radius="md" />
+		<SimpleGrid cols={3} className={classes.board}>
+			<Stack className={classes.panel} pt={20}>
+				<Skeleton p={30} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
 			</Stack>
-			<Stack miw={300}>
-				<Skeleton height={50} radius="md" />
-				<Skeleton height={50} radius="md" />
-				<Skeleton height={50} radius="md" />
+			<Stack className={classes.panel} pt={20}>
+				<Skeleton p={30} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
 			</Stack>
-			<Stack miw={300}>
-				<Skeleton height={50} radius="md" />
-				<Skeleton height={50} radius="md" />
-				<Skeleton height={50} radius="md" />
+			<Stack className={classes.panel} pt={20}>
+				<Skeleton p={30} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
+				<Skeleton className={classes.card} />
 			</Stack>
 		</SimpleGrid>
 	);
